b1f23e5096e2e2f6abdc414dea96dfe80682323d
Shawn Boyette <shawn@firepear.net>
2021-06-23 22:00:36 -0600

    adding Tagger

--- t.js
+++ t.js
@@ -1,5 +1,7 @@
 // twiglets widget library
 
 import { Completer } from './modules/completer.js'
+import { Tagger } from './modules/tagger.js'
 
-window.twiglet = { completer: Completer };
+window.twiglet = { completer: Completer,
+                   tagger: Tagger };
